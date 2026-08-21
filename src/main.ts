16e683a52d760f8aa7c92f645aa6c844301f9ed4
Richard van Heel <richard.vanheel@gmail.com>
2026-08-21 13:45:01 +0200

    Load model lab styles

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App.vue'
 import './style.css'
+import './model-lab.css'
 
 registerSW({ immediate: true })
 createApp(App).mount('#app')
